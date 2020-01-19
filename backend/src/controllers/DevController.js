f6185acdd26d51a13ce274de2ec2e660eed45d83
Vinícius Augutis <vinicius_augutis@hotmail.com>
2020-01-19 17:18:56 -0300

    Criando controller para Dev

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+const Dev = require("../models/Dev");
+
+class DevController {
+  async store(req, res) {
+    const { github_username, techs, latitude, longitude } = req.body;
+
+    const apiResponse = await axios.get(
+      `https://api.github.com/users/${github_username}`
+    );
+
+    const { name = login, avatar_url, bio } = apiResponse.data;
+
+    const techsArray = techs.split(",").map(tech => tech.trim());
+
+    const location = {
+      type: "Point",
+      coordinates: [longitude, latitude]
+    };
+
+    const dev = await Dev.create({
+      github_username,
+      name,
+      avatar_url,
+      bio,
+      techs: techsArray,
+      location
+    });
+
+    return res.json(dev);
+  }
+}
+
+module.exports = new DevController();
