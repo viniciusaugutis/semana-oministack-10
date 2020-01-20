09977bd84102399198561925c7aae5824727277e
Vinícius Augutis <vinicius_augutis@hotmail.com>
2020-01-19 22:08:34 -0300

    Criando componentes e estilizando sidebar no ReactJS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,47 @@
 import React, { useState } from "react";
 import Header from "./Header";
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
+
 
 //Componente
 //Estado
 //Propriedade
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function incrementCounter() {
-    setCounter(counter + 1); //seto o valor da variavel para counter + 1
-  }
-
   return (
-    <>
-      <Header title="Dashboard" />
-      <Header title="Dashboard2" />
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCounter}>Incrementar</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main></main>
+    </div>
   );
 }
 
