09da89843cf90c8038dffd084bf425147ebb8ba1
Vinícius Augutis <vinicius_augutis@hotmail.com>
2020-01-19 23:00:45 -0300

    Criando componente para DevForm e passando function para componente

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,11 @@ import "./Sidebar.css";
 import "./Main.css";
 
 import "./components/DevItem";
+import "./components/DevForm";
 
 import api from "./services/api";
 import DevItem from "./components/DevItem";
+import DevForm from "./components/DevForm";
 
 //Componente
 //Estado
@@ -15,26 +17,6 @@ import DevItem from "./components/DevItem";
 
 function App() {
   const [devs, setDevs] = useState([]);
-  const [github_username, setGithubUsername] = useState("");
-  const [techs, setTechs] = useState("");
-  const [latitude, setLatitude] = useState("");
-  const [longitude, setLongitude] = useState("");
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        const { latitude, longitude } = position.coords;
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      err => {
-        console.log(err);
-      },
-      {
-        timeout: 30000 //timeout de 30s na position
-      }
-    );
-  }, []);
 
   useEffect(() => {
     async function loadDevs() {
@@ -45,18 +27,9 @@ function App() {
     loadDevs();
   }, []);
 
-  async function handleAddDev(e) {
-    e.preventDefault();
+  async function handleAddDev(data) {
 
-    const response = await api.post("/devs", {
-      github_username,
-      techs,
-      latitude,
-      longitude
-    });
-
-    setGithubUsername("");
-    setTechs("");
+    const response = await api.post("/devs", data);
 
     setDevs([...devs, response.data]);
   }
@@ -65,56 +38,7 @@ function App() {
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input
-              name="github_username"
-              id="github_username"
-              required
-              value={github_username}
-              onChange={e => setGithubUsername(e.target.value)}
-            />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input
-              name="techs"
-              id="techs"
-              required
-              value={techs}
-              onChange={e => setTechs(e.target.value)}
-            />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input
-                type="number"
-                name="latitude"
-                id="latitude"
-                required
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}
-              />
-            </div>
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input
-                type="number"
-                name="longitude"
-                id="longitude"
-                required
-                value={longitude}
-                onChange={e => setLongitude(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev}/>
       </aside>
 
       <main>
--- frontend/src/components/DevItem/index.js
+++ frontend/src/components/DevItem/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './styles.css'
+import "./styles.css";
 
 function DevItem(props) {
   const { dev } = props;
